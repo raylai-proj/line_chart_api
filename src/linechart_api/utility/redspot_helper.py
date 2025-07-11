057d9f9dfaa678a28deded8baaac0478f0fed360
raylai <chunjueilai@gmail.com>
2025-07-11 17:33:29 +0800

    feat: construct analyze_thermo_images api structure

---
+++ src/linechart_api/utility/redspot_helper.py
@@ -0,0 +1,13 @@
+OPENFILEFOLDEREMOJI = "\U0001f4c2"
+
+
+def find_hottest_region():
+    """check if hottest region is distributed and find the largest hottest region"""
+
+
+def process_image(file_paths):
+    """analyze each images from file paths and draw arrow to point out"""
+
+
+def analyze_thermo_images():
+    """prompt user to select thermographic images"""
