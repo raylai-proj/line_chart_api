d303ba63d5f1084308adff2765e050def1ee4684
raylai <chunjueilai@gmail.com>
2025-07-16 17:43:05 +0800

    docs: add docstrings in main.py

--- src/linechart_api/main.py
+++ src/linechart_api/main.py
@@ -1,9 +1,12 @@
+"""Summary: main.py launch the main GUI window for data analysis."""
+
 import tkinter as tk
 
 from linechart_api import utility
 
 
 def main():
+    """Entry point of the program."""
     window = tk.Tk()
     utility.Components(window)
     window.mainloop()
